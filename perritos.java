0200c4df93b75d1f2ec583bb3c047ea8fcd1de29
Miguel Martinez Rebollo <9202684@alumnos.ufv.es>
2024-10-26 12:46:21 +0200

    Create perritos.java

---
+++ perritos.java
@@ -0,0 +1,14 @@
+package com.ufv.ejemplo01;
+
+public class Perritos {
+    public String getRaza() {
+        return raza;
+    }
+
+    public void setRaza(String raza) {
+        this.raza = raza;
+    }
+
+    private String raza;
+
+}
